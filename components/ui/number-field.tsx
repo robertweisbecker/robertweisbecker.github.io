86c34e2283b203d669a2076a231627efa0fd043d
rpw <rpw@robertweisbecker.com>
2026-07-02 17:19:47 -0700

    perf: round svg path precision

--- components/ui/number-field.tsx
+++ components/ui/number-field.tsx
@@ -135,7 +135,7 @@ export function CursorGrowIcon(props: React.ComponentProps<"svg">): React.ReactE
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M19.5 5.5L6.49737 5.51844V2L1 6.9999L6.5 12L6.49737 8.5L19.5 8.5V12L25 6.9999L19.5 2V5.5Z" />
+      <path d="M19.5 5.5L6.5 5.52V2L1 7L6.5 12L6.5 8.5L19.5 8.5V12L25 7L19.5 2V5.5Z" />
     </svg>
   );
 }
